285ffb85055411da2c0c751a23469eebeda58cc7
Yung Dai <yungchidai@gmail.com>
2015-05-11 15:39:42 -0400

    initial commit

---
+++ AnyWall/PAWWallPostsTableViewController.h
@@ -0,0 +1,27 @@
+//
+//  PAWWallPostsTableViewController.h
+//  Anywall
+//
+//  Copyright (c) 2014 Parse Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+
+#import "PAWWallViewController.h"
+
+@class PAWWallPostsTableViewController;
+
+@protocol PAWWallPostsTableViewControllerDataSource <NSObject>
+
+- (CLLocation *)currentLocationForWallPostsTableViewController:(PAWWallPostsTableViewController *)controller;
+
+@end
+
+@interface PAWWallPostsTableViewController : PFQueryTableViewController <PAWWallViewControllerHighlight>
+
+@property (nonatomic, weak) id<PAWWallPostsTableViewControllerDataSource> dataSource;
+
+@end
